23ff5390117b5cafae5ab935c0e633084d1b2278
Arianna Johnson <ariannajohnson588@gmail.com>
2021-12-09 18:06:33 -0500

    Toggle component basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Intro from "./components/Intro";
 import Projects from "./components/Projects";
+import Toggle from "./components/Toggle";
 
 function App() {
   return (
     <div className="App">
+      <Toggle />
       <Intro />
       <Projects />
     </div>
---
+++ src/components/Toggle/index.js
@@ -0,0 +1,9 @@
+import './Toggle.css';
+
+const Toggle = () => {
+    return (
+        <div className='t'>Toggle</div>
+    )
+};
+
+export default Toggle;
