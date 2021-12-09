4671669cc6256ae01f77ebf4a3b9b38a58fc7e45
Arianna Johnson <ariannajohnson588@gmail.com>
2021-12-09 18:16:15 -0500

    set up button style

--- src/components/Toggle/index.js
+++ src/components/Toggle/index.js
@@ -1,8 +1,14 @@
 import './Toggle.css';
+import Sun from '../../media/sun.png';
+import Moon from '../../media/moon.png';
 
 const Toggle = () => {
     return (
-        <div className='t'>Toggle</div>
+        <div className='t'>
+            <img src={Sun} alt='sun' className='t_icon'/>
+            <img src={Moon} alt='moon' className='t_icon'/>
+            <div className='t_button'></div>
+        </div>
     )
 };
 
