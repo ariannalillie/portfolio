4d464aaffb68e287fc228496b11e88847969c23b
Arianna Johnson <ariannajohnson588@gmail.com>
2021-12-09 18:40:10 -0500

    toggle between light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useContext } from "react";
 import Intro from "./components/Intro";
 import Projects from "./components/Projects";
 import Toggle from "./components/Toggle";
+import { ThemeContext } from "./context";
 
 function App() {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+
 
   return (
-    <div className="App">
+    <div style={{
+      backgroundColor:darkMode ? '#222' : 'white',
+      color:darkMode && 'white'
+    }}>
       <Toggle />
       <Intro />
       <Projects />
--- src/components/Toggle/index.js
+++ src/components/Toggle/index.js
@@ -1,13 +1,21 @@
 import './Toggle.css';
 import Sun from '../../media/sun.png';
 import Moon from '../../media/moon.png';
+import { useContext } from 'react';
+import { ThemeContext } from '../../context';
 
 const Toggle = () => {
+    const theme = useContext(ThemeContext);
+
+    const handleClick = () => {
+        theme.dispatch({type: 'TOGGLE'})
+    };
+
     return (
         <div className='t'>
             <img src={Sun} alt='sun' className='t_icon'/>
             <img src={Moon} alt='moon' className='t_icon'/>
-            <div className='t_button'></div>
+            <div className='t_button' onClick={handleClick} style={{left: theme.state.darkMode ? 0 : 25}} ></div>
         </div>
     )
 };
